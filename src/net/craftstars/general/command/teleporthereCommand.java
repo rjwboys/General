35e623890ba29734232947678d904e36ab66faf1
Nathaniel Michael <natecmichael@gmail.com>
2011-03-01 14:50:45 -0800

    Added teleporthere command and improved plugin.yml

---
+++ src/net/craftstars/general/command/teleporthereCommand.java
@@ -0,0 +1,45 @@
+package net.craftstars.general.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+import net.craftstars.general.General;
+import net.craftstars.general.util.Messaging;
+import net.craftstars.general.util.Teleport;
+import net.craftstars.general.util.Toolbox;
+
+public class teleporthereCommand extends GeneralCommand
+{
+    
+    @Override
+    public boolean fromPlayer(General plugin, Player sender, Command command, String commandLabel, String[] args)
+    {
+        if (args.length < 1 )
+        {
+            return false;
+        }
+        
+        Player player = Toolbox.playerMatch(args[0]);
+        
+        if (player == null)
+        {
+            Messaging.send(sender, "Couldn't find player.");
+            
+            return true;
+        }
+        
+        if (player.equals(sender))
+        {
+            Messaging.send(sender, "You cannot teleport yourself.");
+            
+            return true;
+        }
+        
+        Teleport.teleportPlayerToPlayer(player, sender);
+        
+        Messaging.send(sender, "Teleported "+player.getName()+" to you!");
+        
+        return true;
+    }
+    
+}
