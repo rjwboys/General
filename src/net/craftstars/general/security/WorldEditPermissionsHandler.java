edf4de04d4be18c938ac68c0f53ec50e1943e122
Celtic Minstrel <celtic.minstrel.ca@>
2011-03-08 00:56:18 -0500

    Fixed WorldEdit-based permissions
    Basically, General's WorldEdit handler just checks if WorldEdit is available without actually setting up the permissions. Then, when a permission is actually requested, it asks WorldEdit to set up the permissions.

--- src/net/craftstars/general/security/WorldEditPermissionsHandler.java
+++ src/net/craftstars/general/security/WorldEditPermissionsHandler.java
@@ -6,33 +6,39 @@ import org.bukkit.plugin.Plugin;
 
 import com.sk89q.bukkit.migration.PermissionsResolverManager;
 import com.sk89q.bukkit.migration.PermissionsResolverServerListener;
-
 import net.craftstars.general.General;
 import net.craftstars.general.security.PermissionsHandler;
 
 public class WorldEditPermissionsHandler implements PermissionsHandler {
     private PermissionsResolverManager perms;
+    private PermissionsResolverServerListener permsListener;
     private final String version;
     private boolean wasLoaded = false;
     
     public WorldEditPermissionsHandler() {
         Plugin test = General.plugin.getServer().getPluginManager().getPlugin("WorldEdit");
-        if(test == null) this.version = "0.0";
-        else {
-            perms = new PermissionsResolverManager(General.plugin.getConfiguration(), General.plugin.getServer(),
-                    General.plugin.getDescription().getName(), General.logger.getInternal());
-            (new PermissionsResolverServerListener(perms)).register(General.plugin);
+        if(test != null) {
             this.wasLoaded = true;
             this.version = test.getDescription().getVersion();
-        }
+        } else this.version = "0.0";
+    }
+
+    private void loadPermissions() {
+        perms = new PermissionsResolverManager(General.plugin.getConfiguration(), General.plugin.getServer(),
+                General.plugin.getDescription().getName(), General.logger.getInternal());
+        permsListener = new PermissionsResolverServerListener(perms); 
+        permsListener.register(General.plugin);
+        perms.load();
     }
     
     public boolean hasPermission(Player who, String what) {
+        if(this.perms == null) loadPermissions();
         return perms.hasPermission(who.getName(), what);
     }
 
     public boolean inGroup(Player who, String which) {
         if(which == ".isop") return who.isOp();
+        if(this.perms == null) loadPermissions();
         return perms.inGroup(who.getName(), which);
     }
 
