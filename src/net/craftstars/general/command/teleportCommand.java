6febe2708e4313a28274790f27154cb6eba8172e
Celtic Minstrel <celtic.minstrel.ca@>
2011-03-29 23:33:22 -0400

    Added multiworld support to the /teleport command - You can now specify a world as the destination

--- src/net/craftstars/general/command/teleportCommand.java
+++ src/net/craftstars/general/command/teleportCommand.java
@@ -2,6 +2,7 @@
 package net.craftstars.general.command;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -19,9 +20,14 @@ public class teleportCommand extends CommandBase {
             String[] args) {
         if(Toolbox.lacksPermission(plugin, sender, "teleport", "general.teleport")) return true;
         switch(args.length) {
-        case 1: {// /tp <player>
+        case 1: {// /tp <player|world>
             Player destination = Toolbox.getPlayer(args[0], sender);
-            if(destination == null) return true;
+            if(destination == null) {
+                World where = Toolbox.getWorld(args[0], sender);
+                if(where == null) return true;
+                sender.teleport(where.getSpawnLocation());
+                return true;
+            }
             if(destination.getName().equals(sender.getName())) {
                 Messaging.send(sender, "&fCongrats! You just teleported yourself to yourself!");
                 return true;
@@ -31,10 +37,14 @@ public class teleportCommand extends CommandBase {
             Messaging.send(sender, "&fTeleported you to &9" + destination.getName() + "&f!");
         }
         break;
-        case 2: { // /tp <player> <to-player>
+        case 2: { // /tp <player> <to-player|world>
             Player destination = Toolbox.getPlayer(args[1], sender);
-            if(destination == null) return true;
-
+            if(destination == null) {
+                World where = Toolbox.getWorld(args[1], sender);
+                if(where == null) return true;
+                sender.teleport(where.getSpawnLocation());
+                return true;
+            }
             if(args[0].equalsIgnoreCase("*")) {
                 if(Toolbox.lacksPermission(plugin, sender, "teleport many players at once", "general.teleport.other.mass")) return true;
                 Teleport.teleportAllToPlayer(destination);
@@ -79,12 +89,17 @@ public class teleportCommand extends CommandBase {
                     + args[2] + ")&f!");
         }
         break;
-        case 4: { // /tp <player> <x> <y> <z>
+        case 4: { // /tp <player> <x> <y> <z> OR /tp <x> <y> <z> <world>
             if(Toolbox.lacksPermission(plugin, sender, "teleport to coordinates", "general.teleport.coords")) return true;
             Player who = Toolbox.getPlayer(args[0], sender);
-            if(who == null) return true;
-            Location destination =
-                    Toolbox.getLocation(sender, who.getWorld(), args[1], args[2], args[3]);
+            Location destination;
+            if(who == null) {
+                World where = Toolbox.getWorld(args[3], sender);
+                if(where == null) return true;
+                destination = Toolbox.getLocation(sender, where, args[0], args[1], args[2]);
+            } else {
+                destination = Toolbox.getLocation(sender, who.getWorld(), args[1], args[2], args[3]);
+            }
             if(destination == null) return true;
             if(!who.getName().equals(sender.getName())) {
                 if(Toolbox.lacksPermission(plugin, sender, "teleport players other than yourself", "general.teleport.other")) return true;
@@ -96,6 +111,23 @@ public class teleportCommand extends CommandBase {
                     + args[1] + "," + args[2] + ")&f!");
         }
         break;
+        case 5: { // /tp <player> <x> <y> <z> <world>
+            if(Toolbox.lacksPermission(plugin, sender, "teleport to coordinates", "general.teleport.coords")) return true;
+            Player who = Toolbox.getPlayer(args[0], sender);
+            if(who == null) return true;
+            World where = Toolbox.getWorld(args[3], sender);
+            if(where == null) return true;
+            Location destination = Toolbox.getLocation(sender, where, args[0], args[1], args[2]);
+            if(destination == null) return true;
+            if(!who.getName().equals(sender.getName())) {
+                if(Toolbox.lacksPermission(plugin, sender, "teleport players other than yourself", "general.teleport.other")) return true;
+                Messaging.send(who, "&fYou have been teleported to &9(" + args[0] + "," + args[1]
+                        + "," + args[2] + ")&f!");
+            }
+            who.teleport(destination);
+            Messaging.send(sender, "&fTeleported &9" + who.getName() + "&f to &9(" + args[0] + ","
+                    + args[1] + "," + args[2] + ")&f!");
+        }
         default:
             return Toolbox.SHOW_USAGE;
         }
@@ -106,29 +138,35 @@ public class teleportCommand extends CommandBase {
     public boolean fromConsole(General plugin, CommandSender sender, Command command,
             String commandLabel, String[] args) {
         switch(args.length) {
-        case 2: { // tp <player> <to-player>
+        case 2: { // tp <player> <to-player|world>
             Player destination = Toolbox.getPlayer(args[1], sender);
-            if(destination == null) return true;
-
-            if(args[0].equalsIgnoreCase("*")) {
-                Teleport.teleportAllToPlayer(destination);
-                Messaging.send(sender, "&fTeleported all players to &9" + destination.getName()
-                        + "&f!");
-                return true;
-            }
-
-            if(args[0].contains(",")) {
-                Teleport.teleportManyToPlayer(args[0], destination);
-                Messaging.send(sender, "&fTeleported several players to &9" + destination.getName()
-                        + "&f!");
-                return true;
+            World where = null;
+            if(destination == null) {
+                where = Toolbox.getWorld(args[1], sender);
+                if(where == null) return true;
+            } else {
+                if(args[0].equalsIgnoreCase("*")) {
+                    Teleport.teleportAllToPlayer(destination);
+                    Messaging.send(sender, "&fTeleported all players to &9" + destination.getName()
+                            + "&f!");
+                    return true;
+                }
+    
+                if(args[0].contains(",")) {
+                    Teleport.teleportManyToPlayer(args[0], destination);
+                    Messaging.send(sender, "&fTeleported several players to &9" + destination.getName()
+                            + "&f!");
+                    return true;
+                }
             }
 
             Player who = Toolbox.getPlayer(args[0], sender);
             if(who == null) return true;
             Messaging.send(who, "&fYou have been teleported to &9" + destination.getName()
                         + "&f!");
-            Teleport.teleportPlayerToPlayer(who, destination);
+            if(destination != null)
+                Teleport.teleportPlayerToPlayer(who, destination);
+            else who.teleport(where.getSpawnLocation());
             Messaging.send(sender, "&fTeleported &9" + who.getName() + "&f to &9"
                     + destination.getName() + "&f!");
         }
@@ -146,6 +184,19 @@ public class teleportCommand extends CommandBase {
                     + args[2] + "," + args[3] + ")&f!");
         }
         break;
+        case 5: { // /tp <player> <x> <y> <z> <world>
+            Player who = Toolbox.getPlayer(args[0], sender);
+            if(who == null) return true;
+            World where = Toolbox.getWorld(args[3], sender);
+            if(where == null) return true;
+            Location destination = Toolbox.getLocation(sender, where, args[0], args[1], args[2]);
+            if(destination == null) return true;
+            Messaging.send(who, "&fYou have been teleported to &9(" + args[0] + "," + args[1]
+                    + "," + args[2] + ")&f!");
+            who.teleport(destination);
+            Messaging.send(sender, "&fTeleported &9" + who.getName() + "&f to &9(" + args[0] + ","
+                    + args[1] + "," + args[2] + ")&f!");
+        }
         default:
             return Toolbox.SHOW_USAGE;
         }
