9b9c71bd953fd010befe9fb10db781e39951b145
Celtic Minstrel <celtic.minstrel.ca@some.place>
2012-02-23 01:42:46 -0500

    Rework /who command to use an EnumSet instead of a bit mask

--- src/net/craftstars/general/command/info/whoCommand.java
+++ src/net/craftstars/general/command/info/whoCommand.java
@@ -4,8 +4,10 @@ package net.craftstars.general.command.info;
 
 import static java.lang.Math.rint;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -22,26 +24,19 @@ import net.craftstars.general.text.Messaging;
 import net.craftstars.general.util.Option;
 import net.craftstars.general.util.Toolbox;
 
+import static net.craftstars.general.command.info.whoCommand.Property.*;
+
 public class whoCommand extends CommandBase {
-	private static final int UNAME = 1 << 0;
-	private static final int DNAME = 1 << 1;
-	private static final int HEALTH = 1 << 2;
-	private static final int LOC = 1 << 3;
-	private static final int HOME = 1 << 4;
-	private static final int WORLD = 1 << 5;
-	private static final int IP = 1 << 6;
-	private static final int STATUS = 1 << 7;
-	private static final int TITLE = 1 << 8;
-	private static final int LEVEL = 1 << 9;
-	private final int defaultMask;
+	enum Property {UNAME, DNAME, HEALTH, LOC, HOME, WORLD, IP, STATUS, TITLE, LEVEL}
+	private final Set<Property> defaultMask;
 	
 	public whoCommand(General instance) {
 		super(instance);
-		int mask = TITLE | UNAME | DNAME | STATUS | LEVEL;
-		if(Option.SHOW_HEALTH.get()) mask |= HEALTH;
-		if(Option.SHOW_COORDS.get()) mask |= LOC | HOME;
-		if(Option.SHOW_WORLD.get()) mask |= WORLD;
-		if(Option.SHOW_IP.get()) mask |= IP;
+		Set<Property> mask = EnumSet.of(TITLE, UNAME, DNAME, STATUS, LEVEL);
+		if(Option.SHOW_HEALTH.get()) mask.add(HEALTH);
+		if(Option.SHOW_COORDS.get()) mask.addAll(EnumSet.of(LOC, HOME));
+		if(Option.SHOW_WORLD.get()) mask.add(WORLD);
+		if(Option.SHOW_IP.get()) mask.add(IP);
 		defaultMask = mask;
 	}
 	
@@ -51,43 +46,43 @@ public class whoCommand extends CommandBase {
 		return LanguageText.INFO_HEALTHBAR.value("value", health, "health", full, "filler", empty);
 	}
 
-	private void showInfo(Player ofWhom, CommandSender toWhom, int mask) {
+	private void showInfo(Player ofWhom, CommandSender toWhom, Set<Property> mask) {
 		LanguageText divider = LanguageText.INFO_DIVIDER;
-		boolean title = (mask & TITLE) > 0;
+		boolean title = mask.contains(TITLE);
 		if(title) {
 			Messaging.send(toWhom, divider);
 			Messaging.send(toWhom, LanguageText.INFO_TITLE_PLAYER.value("name", ofWhom.getName()));
 			Messaging.send(toWhom, divider);
 		}
-		if((mask & UNAME) > 0)
+		if(mask.contains(UNAME))
 			Messaging.send(toWhom, LanguageText.INFO_USERNAME.value("name", ofWhom.getName()));
-		if((mask & DNAME) > 0)
+		if(mask.contains(DNAME))
 			Messaging.send(toWhom, LanguageText.INFO_DISPLAYNAME.value("name", ofWhom.getDisplayName()));
-		if((mask & HEALTH) > 0) {
+		if(mask.contains(HEALTH)) {
 			double health = ofWhom.getHealth() / 2.0;
 			String bar = getHealthBar(health);
 			Messaging.send(toWhom, LanguageText.INFO_HEALTH.value("bar", bar, "value", health));
 		}
-		if((mask & LEVEL) > 0) {
+		if(mask.contains(LEVEL)) {
 			float percent = (float)(rint(ofWhom.getExp() * 1000)) / 10.0f;
 			Messaging.send(toWhom, LanguageText.INFO_LEVEL.value("lvl", ofWhom.getLevel(), "percent", percent));
 			Messaging.send(toWhom, LanguageText.INFO_XP.value("xp", ofWhom.getTotalExperience(), "percent", percent));
 		}
-		if((mask & LOC) > 0) {
+		if(mask.contains(LOC)) {
 			Location loc = ofWhom.getLocation();
 			Messaging.send(toWhom, LanguageText.INFO_LOCATION.value("location", Toolbox.formatLocation(loc)));
 		}
-		if((mask & HOME) > 0) {
+		if(mask.contains("HOME")) {
 			Location home = Destination.homeOf(ofWhom).getLoc();
 			Messaging.send(toWhom, LanguageText.INFO_SPAWN.value("location", Toolbox.formatLocation(home)));
 		}
-		if((mask & WORLD) > 0)
+		if(mask.contains(WORLD))
 			Messaging.send(toWhom, LanguageText.INFO_WORLD.value("world", ofWhom.getWorld().getName()));
-		if((mask & IP) > 0 && canSeeIp(ofWhom, toWhom)) {
+		if(mask.contains(IP) && canSeeIp(ofWhom, toWhom)) {
 			String ip = ofWhom.getAddress().getAddress().getHostAddress();
 			Messaging.send(toWhom, LanguageText.INFO_IP.value("ip", ip));
 		}
-		if((mask & STATUS) > 0) {
+		if(mask.contains(STATUS)) {
 			String status;
 			if(General.players.isAway(ofWhom))
 				status = LanguageText.INFO_STATUS_AWAY.value("away", General.players.whyAway(ofWhom));
@@ -97,43 +92,44 @@ public class whoCommand extends CommandBase {
 		if(title) Messaging.send(toWhom, divider);
 	}
 	
-	private void showInfo(ConsoleCommandSender ofWhom, CommandSender toWhom, int mask) {
+	private void showInfo(ConsoleCommandSender ofWhom, CommandSender toWhom, Set<Property> mask) {
 		LanguageText divider = LanguageText.INFO_DIVIDER;
-		boolean title = (mask & TITLE) > 0;
+		boolean title = mask.contains(TITLE);
 		if(title) {
 			Messaging.send(toWhom, divider);
 			Messaging.send(toWhom, LanguageText.INFO_TITLE_CONSOLE);
 			Messaging.send(toWhom, divider);
 		}
-		if((mask & UNAME) > 0)
+		if(mask.contains(UNAME))
 			Messaging.send(toWhom, LanguageText.INFO_USERNAME.value("name", ofWhom.getName()));
-		if((mask & DNAME) > 0)
-			Messaging.send(toWhom, LanguageText.INFO_DISPLAYNAME.value("name", "Server"));
-		int healthloc = HEALTH | LOC | HOME;
-		if((mask & healthloc) > 0)
+		if(mask.contains(DNAME))
+			Messaging.send(toWhom, LanguageText.INFO_DISPLAYNAME.value("name", "Server");
+		Set<Property> healthloc = EnumSet.of(HEALTH, LOC, HOME);
+		if(mask.containsAll(healthloc))
 			Messaging.send(toWhom, LanguageText.INFO_VERSION.value("version", Bukkit.getVersion()));
-		if((mask & WORLD) > 0)
+		if(mask.contains(WORLD))
 			Messaging.send(toWhom, LanguageText.INFO_PRIMARY.value("world", Bukkit.getWorlds().get(0).getName()));
-		if((mask & IP) > 0) {
+		if(mask.contains(IP)) {
 			String ip = Bukkit.getIp();
 			int port = Bukkit.getPort();
 			if(ip.isEmpty()) ip = "<???>";
 			Messaging.send(toWhom, LanguageText.INFO_IP.value("ip", ip + ":" + port));
 		}
-		if((mask & STATUS) > 0)
+		if(mask.contains(STATUS))
 			Messaging.send(toWhom, LanguageText.INFO_STATUS.value("status", LanguageText.INFO_STATUS_SERVER.value()));
 		if(title) Messaging.send(toWhom, divider);
 	}
 	
-	private void showInfo(CommandSender ofWhom, CommandSender toWhom, int mask) {
+	private void showInfo(CommandSender ofWhom, CommandSender toWhom, Set<Property> mask) {
 		LanguageText divider = LanguageText.INFO_DIVIDER;
-		boolean title = (mask & TITLE) > 0;
+		boolean title = mask.contains(TITLE);
+		mask.remove(TITLE);
 		if(title) {
 			Messaging.send(toWhom, divider);
 			Messaging.send(toWhom, LanguageText.INFO_TITLE_UNKNOWN);
 			Messaging.send(toWhom, divider);
 		}
-		if((mask &~ TITLE) > 0) Messaging.send(toWhom, LanguageText.INFO_NAME.value("name", ofWhom.getName()));
+		if(!mask.isEmpty()) Messaging.send(toWhom, LanguageText.INFO_NAME.value("name", ofWhom.getName()));
 		if(title) Messaging.send(toWhom, divider);
 	}
 	
@@ -153,20 +149,19 @@ public class whoCommand extends CommandBase {
 		if(args.length > 1) return null;
 		HashMap<String, Object> params = new HashMap<String,Object>();
 		if(label.equalsIgnoreCase("whoami") || args.length == 0) {
-			int mask = 0;
+			Set<Property> mask = EnumSet.noneOf(Property.class);
 			if(args.length == 1) {
-				if(Toolbox.equalsOne(args[0], "all", "uname", "name", "username")) mask |= UNAME;
-				if(Toolbox.equalsOne(args[0], "all", "dname", "name", "displayname", "nick", "nickname")) mask |= DNAME;
-				if(Toolbox.equalsOne(args[0], "all", "health", "hp", "version")) mask |= HEALTH;
-				if(Toolbox.equalsOne(args[0], "all", "loc", "location", "pos", "position", "coords")) mask |= LOC;
-				if(Toolbox.equalsOne(args[0], "all", "home", "spawn", "bed")) mask |= HOME;
-				if(Toolbox.equalsOne(args[0], "all", "world", "worldname")) mask |= WORLD;
-				if(Toolbox.equalsOne(args[0], "all", "ip", "address")) mask |= IP;
-				if(Toolbox.equalsOne(args[0], "all", "status", "away")) mask |= STATUS;
+				if(Toolbox.equalsOne(args[0], "all", "uname", "name", "username")) mask.add(UNAME);
+				if(Toolbox.equalsOne(args[0], "all", "dname", "name", "displayname", "nick", "nickname")) mask.add(DNAME);
+				if(Toolbox.equalsOne(args[0], "all", "health", "hp", "version")) mask.add(HEALTH);
+				if(Toolbox.equalsOne(args[0], "all", "loc", "location", "pos", "position", "coords")) mask.add(LOC);
+				if(Toolbox.equalsOne(args[0], "all", "home", "spawn", "bed")) mask.add(HOME);
+				if(Toolbox.equalsOne(args[0], "all", "world", "worldname")) mask.add(WORLD);
+				if(Toolbox.equalsOne(args[0], "all", "ip", "address")) mask.add(IP);
+				if(Toolbox.equalsOne(args[0], "all", "status", "away")) mask.add(STATUS);
 			} else mask = defaultMask;
 			// If they're not allowed to override, mask out disabled values
-			if(!Option.ALLOW_OVERRIDE.get()) mask &= defaultMask;
-			General.logger.debug(Integer.toString(mask));
+			if(!Option.ALLOW_OVERRIDE.get()) mask.retainAll(defaultMask);
 			params.put("mask", mask);
 			params.put("who", sender);
 		} else {
@@ -186,7 +181,8 @@ public class whoCommand extends CommandBase {
 		CommandSender who = (CommandSender) args.get("who");
 		if(!who.equals(sender) && !sender.hasPermission("general.who"))
 			return Messaging.lacksPermission(sender, "general.who");
-		int mask = (Integer) args.get("mask");
+		@SuppressWarnings("unchecked")
+		Set<Property> mask = (Set<Property>) args.get("mask");
 		if(who instanceof Player) showInfo((Player) who, sender, mask);
 		else if(who instanceof ConsoleCommandSender)
 			showInfo((ConsoleCommandSender) who, sender, mask);
